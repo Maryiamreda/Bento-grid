4cc17ddcc237c19a691b3cf32cbebbf23b12ebe0
Maryiamreda <maryiamreda@gmail.com>
2024-12-15 22:18:26 +0200

    kinda works

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.css';
 function App() {
   // grid-cols-4 grid-rows-4
   return (
-    <div className="grid  gap-6">
-      <div className="bg-yellow-100 row-span-2 ">Create</div>
+    <div className="gridbox ">
+      <div className="bg-yellow-100 h-[80px]  ">Create</div>
       <div className="bg-purple-500 col-span-2 h- ">Social Media</div>
-      <div className="bg-lime-200 row-span-3 ">Schedule</div>
-      <div className="bg-yellow-500 row-start-3 row-span-2 ">Write</div>
-      <div className="bg-white ">Manage</div>
-      <div className="bg-yellow-500 ">Maintain</div>
-      <div className="bg-white row-start-4 ">Faster</div>
+      <div className="bg-lime-200 row-span-2 col-start-4 ">Schedule</div>
+      <div className="bg-yellow-500 h-[80px] col-start-1 row-start-2 mt-5  ">Write</div>
+      <div className="bg-white row-start-2 col-start-2 ">Manage</div>
+      <div className="bg-yellow-500 row-start-2 col-start-3 ">Maintain</div>
+      <div className="bg-white row-start-3 col-start-2 ">Faster</div>
 
-      <div className="bg-purple-500 row-start-4 col-span-2 ">Grow</div>
+      <div className="bg-purple-500 row-start-3 col-start-3 col-span-2 ">Grow</div>
 
     </div>
   );
