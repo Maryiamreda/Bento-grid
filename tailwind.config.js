06116d2874de33a9ad309fd067eee209a6b5aa30
Maryiamreda <maryiamreda@gmail.com>
2024-12-15 23:52:15 +0200

    adjust the sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-
-      }
+        purple: {
+          100: 'hsl(254, 88%, 90%)',
+          500: 'hsl(256, 67%, 59%)',
+        },
+        yellow: {
+          100: 'hsl(31, 66%, 93%)',
+          500: 'hsl(39, 100%, 71%)',
+        },
+      },
+      gridTemplateColumns: {
+        // Define a custom 4-column layout with 18vw width each
+        '4-cols-18vw': 'repeat(4, 200px)',
+      },
     },
   },
   plugins: [],
