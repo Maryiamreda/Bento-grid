373fd8d0426dd7075a2f8c9b160ae0386e2cbd3c
Maryiamreda <maryiamreda@gmail.com>
2024-12-16 19:14:21 +0200

    assets issue ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Create from '../public/assets/images/illustration-create-post.webp'
+import Create from './assets/illustration-create-post.webp'
 function App() {
   // grid-cols-4 grid-rows-4
   return (
@@ -14,14 +14,14 @@ function App() {
           <span className='italic'>Faster with  AI</span>
         </p>
 
-        <img src='../public/assets/images/illustration-five-stars.webp' className='w-36' />
+        <img src='./assets/images/illustration-five-stars.webp' className='w-36' />
         <p className='text-sm opacity-85' >   Over 4,000 5-star reviews
         </p>
       </div>
       <div className="bg-purple-200 row-span-2 col-start-4 text-start overflow-hidden  ">
         <p className='text-2xl  font-semibold leading-5 p-5 '>  Schedule to<br /> social media.
         </p>
-        <img src='../public/assets/images/illustration-schedule-posts.webp'
+        <img src='./assets/images/illustration-schedule-posts.webp'
           className=' w-[280px] max-w-[280px] relative left-[25px] shadow-sm ' />
         <p className='text-sm opacity-85 p-5'>  Optimize post timings <br /> to publish content at <br /> the perfect time for <br /> your audience.
         </p>
@@ -29,23 +29,23 @@ function App() {
       <div className="bg-yellow-500 md:h-[325px] col-start-1 row-start-2 mt-20 text-start p-5   ">
         <p className=' text-2xl leading-6 font-semibold '>  Write your<br /> content<br /> using AI.
         </p>
-        <img src='../public/assets/images/illustration-ai-content.webp' className='w-44 max-w-max pt-11' />
+        <img src='./assets/images/illustration-ai-content.webp' className='w-44 max-w-max pt-11' />
       </div>
       <div className="bg-white row-start-2 col-start-2  p-5 overflow-hidden">
-        <img src='../public/assets/images/illustration-multiple-platforms.webp ' className='w-56 max-w-max' />
+        <img src='./assets/images/illustration-multiple-platforms.webp ' className='w-56 max-w-max' />
 
         <p className=' text-xl text-start  opacity-90 leading-5 font-semibold '>  Manage<br /> multiple<br /> accounts and <br />platforms.
         </p>
       </div>
       <div className="bg-yellow-500 row-start-2 col-start-3 p-5  overflow-hidden">
         <p className=' text-xl text-start  opacity-90 leading-5 font-semibold '>  Maintain a consistent<br /> posting<br /> schedule.        </p>
-        <img src='../public/assets/images/illustration-consistent-schedule.webp ' className='w-36 max-w-max md:mt-7' />
+        <img src='./assets/images/illustration-consistent-schedule.webp ' className='w-36 max-w-max md:mt-7' />
 
       </div>
       <div className="bg-white row-start-3 col-start-2 flex flex-col justify-center items-center gap-2">
         <h1 className='font-medium text-5xl'>&gt;56%</h1>
         <p className='text-sm font-medium opacity-95 p-1'>faster audience growth</p>
-        <img src='../public/assets/images/illustration-audience-growth.webp ' className='w-32 max-w-max' />
+        <img src='./assets/images/illustration-audience-growth.webp ' className='w-32 max-w-max' />
 
       </div>
 
